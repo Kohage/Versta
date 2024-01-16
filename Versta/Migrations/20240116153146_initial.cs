51579371bf668daed641748e68d01987dc571bae
Kohage <89828621077@bk.ru>
2024-01-16 18:42:30 +0300

    Добавьте файлы проекта.

---
+++ Versta/Migrations/20240116153146_initial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Versta.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SendCity = table.Column<string>(type: "text", nullable: true),
+                    SendAdress = table.Column<string>(type: "text", nullable: true),
+                    ArriveCity = table.Column<string>(type: "text", nullable: true),
+                    ArriveAdress = table.Column<string>(type: "text", nullable: true),
+                    Weight = table.Column<double>(type: "double precision", nullable: false),
+                    PickupDate = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
---
+++ Versta/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿#nullable disable
+namespace Versta.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string SendCity { get; set; }
+        public string SendAdress { get; set; }
+        public string ArriveCity { get; set; }
+        public string ArriveAdress { get; set; }
+        public double Weight { get; set;}
+        public DateOnly PickupDate { get; set; }
+    }
+}
