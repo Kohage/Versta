674211bf6eb5a225c329a0bb37a42652360cd835
Kohage <89828621077@bk.ru>
2024-01-16 22:44:29 +0300

    Add onclick for table line

--- Versta/Models/Order.cs
+++ Versta/Models/Order.cs
@@ -1,14 +1,22 @@
 ﻿#nullable disable
+using System.ComponentModel.DataAnnotations;
+
 namespace Versta.Models
 {
     public class Order
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Поле обязательно")]
         public string SendCity { get; set; }
+        [Required(ErrorMessage = "Поле обязательно")]
         public string SendAdress { get; set; }
+        [Required(ErrorMessage = "Поле обязательно")]
         public string ArriveCity { get; set; }
+        [Required(ErrorMessage = "Поле обязательно")]
         public string ArriveAdress { get; set; }
+        [Required(ErrorMessage = "Поле обязательно")]
         public double Weight { get; set;}
+        [Required(ErrorMessage = "Поле обязательно")]
         public DateOnly PickupDate { get; set; }
     }
 }
